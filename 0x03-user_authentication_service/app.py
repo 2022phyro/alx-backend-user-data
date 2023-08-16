ebb9add716ae034ab3ab4cbf8707e36848cf7545
2022phyro <phyrokelstein2@gmail.com>
2023-08-16 14:21:25 +0100

    no 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,6 +15,7 @@ def greet():
 
 @app.route("/users", methods=['POST'], strict_slashes=False)
 def signup():
+    """Signs up a user"""
     em = request.form.get('email')
     pwd = request.form.get('password')
     try:
@@ -22,6 +23,20 @@ def signup():
         return jsonify({"email": em, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """"Logs in a user"""
+    em = request.form.get('email')
+    pwd = request.form.get('password')
+    valid = AUTH.valid_login(em, pwd)
+    if not valid:
+        abort(401)
+    s_id = AUTH.create_session(em)
+    res = jsonify({'email': em, 'message': 'logged in'})
+    res.set_cookie('session_id', s_id)
+    return res
 
 
 if __name__ == "__main__":
