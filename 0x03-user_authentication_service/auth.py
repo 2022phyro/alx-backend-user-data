afd296c84a6a18e0ba417252c339ca6589ae9a77
2022phyro <phyrokelstein2@gmail.com>
2023-08-16 15:14:40 +0100

    Still on no 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,6 +85,8 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Updates a password and resets it"""
+        if reset_token is None or password is None:
+            return None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             pwd = _hash_password(password)
