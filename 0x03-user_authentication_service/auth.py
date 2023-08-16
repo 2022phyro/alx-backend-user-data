5501a8c7ad677e2bb55f2908fad203ff1c820b6e
2022phyro <phyrokelstein2@gmail.com>
2023-08-16 14:57:09 +0100

    No 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroys a user session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Creates a new reset_token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
